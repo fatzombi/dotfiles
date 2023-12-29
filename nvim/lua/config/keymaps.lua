8d6cb55f33c1e4b2e6727485f04fb9276f528a82
Phil Thomas <phil@fatzombi.com>
2023-12-28 20:04:22 -0500

    changed from true-zen to zen-mode, added word count to lualine, and updated keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -40,9 +40,7 @@ mapkey("<leader>pa", "echo expand('%:p')", "n") -- Show Full File Path
 local api = vim.api
 
 -- Zen Mode
-api.nvim_set_keymap("n", "<leader>zn", ":TZNarrow<CR>", {})
-api.nvim_set_keymap("v", "<leader>zn", ":'<,'>TZNarrow<CR>", {})
-api.nvim_set_keymap("n", "<leader>sm", ":TZFocus<CR>", {})
+api.nvim_set_keymap("n", "<leader>zm", ":ZenMode<CR>", {})
 
 -- Comments
 api.nvim_set_keymap("n", "<C-_>", "gtc", { noremap = false })
--- nvim/lua/plugins/lualine-nvim.lua
+++ nvim/lua/plugins/lualine-nvim.lua
@@ -8,8 +8,26 @@ local config = function()
 			lualine_a = { "mode" },
 			lualine_b = { "buffers" },
 			lualine_x = { "encoding", "fileformat", "filetype" },
-			lualine_y = { "progress" },
-			lualine_z = { "location" },
+			lualine_y = { "progress", "location" },
+			lualine_z = {
+        {
+          function()
+            local words = vim.fn.wordcount()['words']
+            return 'Words: ' .. words
+          end,
+          cond = function()
+            local ft = vim.opt_local.filetype:get()
+            local count = {
+                  latex = true,
+                  tex = true,
+                  text = true,
+                  markdown = true,
+                  vimwiki = true,
+              }
+              return count[ft] ~= nil
+          end,
+        }
+      },
 		},
 		sections = {},
 	})
