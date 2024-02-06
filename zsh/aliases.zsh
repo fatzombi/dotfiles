ff877558a86f0fd7f2e23a4077382a394222ce4f
Phil Thomas <phil@fatzombi.com>
2024-02-05 20:11:51 -0500

    updated `pubkey` path

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -10,7 +10,7 @@ alias d-c='docker-compose $*'
 alias chmox='chmod +x'
 
 # Pipe my public key to my clipboard. Stolen from https://github.com/holman
-alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+alias pubkey="more ~/.ssh/id_ed25519.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
 
 # Open our "zettelkasten"
 alias z='nvim ~/src/zet/'
