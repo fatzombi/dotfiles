28d0073cece91fd02df4c02cc3b225db286fbeeb
Phil Thomas <phil@fatzombi.com>
2025-03-30 05:37:18 -0400

    i've been ricing

---
+++ borders/join_borders.sh
@@ -0,0 +1,3 @@
+source $HOME/.config/borders/variables.sh
+
+borders active_color="gradient(top_left=0xee$MOCHA_YELLOW,bottom_right=0xee$MOCHA_PEACH)"
---
+++ borders/resize_borders.sh
@@ -0,0 +1,3 @@
+source $HOME/.config/borders/variables.sh
+
+borders active_color="gradient(top_left=0xee$MOCHA_GREEN,bottom_right=0xee$MOCHA_TEAL)"
